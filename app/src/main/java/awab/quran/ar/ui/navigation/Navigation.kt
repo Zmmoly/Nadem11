8976e7de613f94d516e2c06a852bb4f6f60b42aa
Zmmoly <zmmoly@gmail.com>
2026-02-15 13:51:49 +0200

    Update Navigation.kt

--- app/src/main/java/awab/quran/ar/ui/navigation/Navigation.kt
+++ app/src/main/java/awab/quran/ar/ui/navigation/Navigation.kt
@@ -1,115 +1,61 @@
 package awab.quran.ar.ui.navigation
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import awab.quran.ar.ui.screens.auth.LoginScreen
-import awab.quran.ar.ui.screens.auth.RegisterScreen
-import awab.quran.ar.ui.screens.auth.ForgotPasswordScreen
 import awab.quran.ar.ui.screens.home.HomeScreen
-import awab.quran.ar.ui.screens.splash.SplashScreen
-import awab.quran.ar.ui.screens.recitation.RecitationScreen
+import awab.quran.ar.ui.screens.home.Surah
+import awab.quran.ar.ui.screens.surah.SurahScreen
 import awab.quran.ar.ui.screens.profile.ProfileScreen
-
-sealed class Screen(val route: String) {
-    object Splash : Screen("splash")
-    object Login : Screen("login")
-    object Register : Screen("register")
-    object ForgotPassword : Screen("forgot_password")
-    object Home : Screen("home")
-    object Recitation : Screen("recitation")
-    object Profile : Screen("profile")
-}
+import awab.quran.ar.ui.screens.recitation.RecitationScreen
 
 @Composable
-fun NadeemNavigation() {
-    val navController = rememberNavController()
-
+fun Navigation(navController: NavHostController) {
+    var selectedSurah by remember { mutableStateOf<Surah?>(null) }
+    
     NavHost(
         navController = navController,
-        startDestination = Screen.Splash.route
+        startDestination = "home"
     ) {
-        composable(Screen.Splash.route) {
-            SplashScreen(
-                onNavigateToLogin = {
-                    navController.navigate(Screen.Login.route) {
-                        popUpTo(Screen.Splash.route) { inclusive = true }
-                    }
-                },
-                onNavigateToHome = {
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(Screen.Splash.route) { inclusive = true }
-                    }
-                }
-            )
-        }
-
-        composable(Screen.Login.route) {
-            LoginScreen(
-                onNavigateToRegister = {
-                    navController.navigate(Screen.Register.route)
-                },
-                onNavigateToForgotPassword = {
-                    navController.navigate(Screen.ForgotPassword.route)
-                },
-                onLoginSuccess = {
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(Screen.Login.route) { inclusive = true }
-                    }
-                }
-            )
-        }
-
-        composable(Screen.Register.route) {
-            RegisterScreen(
-                onNavigateBack = {
-                    navController.popBackStack()
-                },
-                onRegisterSuccess = {
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(Screen.Login.route) { inclusive = true }
-                    }
-                }
-            )
-        }
-
-        composable(Screen.ForgotPassword.route) {
-            ForgotPasswordScreen(
-                onNavigateBack = {
-                    navController.popBackStack()
-                }
-            )
-        }
-
-        composable(Screen.Home.route) {
+        composable("home") {
             HomeScreen(
                 onNavigateToRecitation = {
-                    navController.navigate(Screen.Recitation.route)
+                    navController.navigate("recitation")
                 },
                 onNavigateToProfile = {
-                    navController.navigate(Screen.Profile.route)
+                    navController.navigate("profile")
+                },
+                onSurahClick = { surah ->
+                    selectedSurah = surah
+                    navController.navigate("surah")
                 }
             )
         }
-
-        composable(Screen.Recitation.route) {
+        
+        composable("surah") {
+            selectedSurah?.let { surah ->
+                SurahScreen(
+                    surah = surah,
+                    onNavigateBack = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+        
+        composable("recitation") {
             RecitationScreen(
                 onNavigateBack = {
                     navController.popBackStack()
                 }
             )
         }
-
-        composable(Screen.Profile.route) {
+        
+        composable("profile") {
             ProfileScreen(
                 onNavigateBack = {
                     navController.popBackStack()
-                },
-                onLogout = {
-                    navController.navigate(Screen.Login.route) {
-                        popUpTo(Screen.Home.route) { inclusive = true }
-                    }
                 }
             )
         }
