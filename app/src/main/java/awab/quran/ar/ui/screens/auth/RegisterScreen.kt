96048f6af5c1bce96d4537b91ac57e105ce759da
Zmmoly <zmmoly@gmail.com>
2026-03-15 00:51:28 +0200

    Update RegisterScreen.kt

--- app/src/main/java/awab/quran/ar/ui/screens/auth/RegisterScreen.kt
+++ app/src/main/java/awab/quran/ar/ui/screens/auth/RegisterScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.foundation.background
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
