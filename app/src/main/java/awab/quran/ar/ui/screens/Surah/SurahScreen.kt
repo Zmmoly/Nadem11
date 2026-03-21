8e78b40df1ec2c66e594834b13b7c801f67a6f0c
Zmmoly <zmmoly@gmail.com>
2026-03-21 22:07:06 +0200

    Update SurahScreen.kt

--- app/src/main/java/awab/quran/ar/ui/screens/Surah/SurahScreen.kt
+++ app/src/main/java/awab/quran/ar/ui/screens/Surah/SurahScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
